377b73674c7d7813d3cc88204981ebc1806f2453
Ranjan Kumar Jha <talk2ranjanktr@gmail.com>
2024-03-22 18:05:07 +0530

    update in Deposit

--- pages/Deposit.js
+++ pages/Deposit.js
@@ -1,29 +1,160 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Text, View} from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useSelector } from 'react-redux'
 
 const Deposit = () => {
-    const username = useSelector(state => state.auth.user.username)
-    const [amount,setAmount]=useState('')
+    const user = useSelector(state => state.auth.user.username)
+    //const user = 'abc'
+    const [amount, setAmount] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [transectionList, setTransectionList] = useState([])
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${URL}/get-calculate-amountOfUser?user=${username}`);
-                setAmount(response.data.data); // Assuming your response contains a 'total_amount' property
+                const response = await axios.get(`https://reactnativeserver.vercel.app/get-calculate-amountOfUser?user=${user}`);
                 console.log(response.data.data)
+                setAmount(response.data.data); // Assuming your response contains a 'total_amount' property
+                setLoading(false)
+
             } catch (error) {
                 console.log('Error fetching data:', error.response.data.message);
             }
         };
 
         fetchData();
-    }, [username]); // Specify username as a dependency to trigger the effect when it changes
-  return (
-    <View>
-        <Text>Deposit Amount:{amount} </Text>
-    </View>
-  )
+    }, [user]); // Specify username as a dependency to trigger the effect when it changes
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://reactnativeserver.vercel.app/get-transection?user=${user}`);
+                console.log(response.data.data)
+                setTransectionList(response.data.data); // Assuming your response contains a 'total_amount' property
+                //setLoading(false)
+
+            } catch (error) {
+                console.log('Error fetching data:', error.response.data.message);
+            }
+        };
+
+        fetchData();
+    }, [user]); // Specify username as a dependency to trigger the effect when it changes
+
+
+    if (loading) {
+        return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+    const UPIList = transectionList.filter(e => e.mode === 'UPI')
+    const UPIAmount = UPIList.reduce((sum, transaction) => sum + transaction.credit_amount, 0);
+    console.log(UPIAmount)
+
+    const cashList = transectionList.filter(e => e.mode === 'Cash')
+    const cashAmount = cashList.reduce((sum, transaction) => sum + transaction.credit_amount, 0);
+    console.log(cashAmount)
+
+    const handleSubmit = () => {
+
+    }
+    const handlePrint = () => {
+
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.amountContainer}>
+                <Text style={[styles.headingtext, { marginTop: 20 }]}>UPI: {UPIAmount}</Text>
+                <Text style={[styles.headingtext, { marginTop: 20 }]}>Cash: {cashAmount}</Text>
+            </View>
+            <Text style={[styles.headingtext, { marginTop: 0 }]}>Deposit Amount: {amount}</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                    <Text style={styles.buttonText}>Update</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handlePrint}>
+                    <Text style={styles.buttonText}>Print</Text>
+                </TouchableOpacity>
+            </View>
+
+
+            {
+                transectionList?.map((item) => {
+                    return (
+                        <View style={styles.dataRow} key={item.tran_id}>
+                            <Text style={styles.dataText}>{item.name}</Text>
+                            <Text style={[styles.dataText, { flex: 2 }]}>{item.group_}</Text>
+                            <Text style={styles.dataText}>{item.mode}</Text>
+                        </View>
+                    );
+                })
+            }
+        </View>
+
+
+    )
 }
 
 export default Deposit
+
+
+const styles = StyleSheet.create({
+    container: {
+        // flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        padding: 16,
+        borderColor: "red"
+    },
+    heading: {
+        fontSize: 24,
+        //fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#0c3761',
+        fontWeight: '500',
+        textAlign: "center"
+    },
+    headingtext: {
+        // Your heading text styles here
+        // For example:
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    dataRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 8,
+        // marginLeft:16,
+    },
+
+    dataText: {
+        fontSize: 16,
+        flex: 1,
+    },
+    amountContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        // marginTop:10
+        // paddingTop:20
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 20
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+        flex: 1,
+        marginRight: 5,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+
+})
